8fe3fb29fa9d3b804015183e254aafd24cc8de9d
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2018-06-26 00:01:41 +0900

    Zoneout LSTM

--- tacotron/modules.py
+++ tacotron/modules.py
@@ -164,3 +164,52 @@ class CBHG(tf.layers.Layer):
 
     def compute_output_shape(self, input_shape):
         return tf.TensorShape([input_shape[0], input_shape[1], self.out_units])
+
+
+class ZoneoutLSTMCell(tf.nn.rnn_cell.RNNCell):
+
+    def __init__(self, num_units, is_training, zoneout_factor_cell=0.0, zoneout_factor_output=0.0, state_is_tuple=True,
+                 name=None):
+        zm = min(zoneout_factor_output, zoneout_factor_cell)
+        zs = max(zoneout_factor_output, zoneout_factor_cell)
+
+        if zm < 0. or zs > 1.:
+            raise ValueError('One/both provided Zoneout factors are not in [0, 1]')
+
+        self._cell = tf.nn.rnn_cell.LSTMCell(num_units, state_is_tuple=state_is_tuple, name=name)
+        self._zoneout_cell = zoneout_factor_cell
+        self._zoneout_outputs = zoneout_factor_output
+        self.is_training = is_training
+        self.state_is_tuple = state_is_tuple
+
+    @property
+    def state_size(self):
+        return self._cell.state_size
+
+    @property
+    def output_size(self):
+        return self._cell.output_size
+
+    def __call__(self, inputs, state, scope=None):
+        # Apply vanilla LSTM
+        output, new_state = self._cell(inputs, state, scope)
+
+        if self.state_is_tuple:
+            (prev_c, prev_h) = state
+            (new_c, new_h) = new_state
+        else:
+            raise NotImplementedError("non-tuple state is not implemented")
+
+        # Apply zoneout
+        keep_rate_cell = 1.0 - self._zoneout_cell
+        keep_rate_output = 1.0 - self._zoneout_outputs
+        if self.is_training:
+            c = keep_rate_cell * tf.nn.dropout(new_c - prev_c, keep_prob=keep_rate_cell) + prev_c
+            h = keep_rate_output * tf.nn.dropout(new_h - prev_h, keep_prob=keep_rate_output) + prev_h
+        else:
+            c = (1.0 - self._zoneout_cell) * new_c + self._zoneout_cell * prev_c
+            h = (1.0 - self._zoneout_outputs) * new_h + self._zoneout_outputs * prev_h
+
+        new_state = tf.nn.rnn_cell.LSTMStateTuple(c, h) if self.state_is_tuple else tf.concat([c, h], axis=1)
+
+        return output, new_state
