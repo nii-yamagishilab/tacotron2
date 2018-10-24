aa4cce515526da13fbd11f62e51b237265d2a50d
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2018-10-24 20:17:31 +0900

    Add license headers

--- datasets/blizzard2012.py
+++ datasets/blizzard2012.py
@@ -1,3 +1,35 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2018, Yamagishi Laboratory, National Institute of Informatics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==============================================================================
+""" Preprocess for Blizzard 2012. """
+
 from pyspark import SparkContext, RDD
 import numpy as np
 import os
@@ -68,7 +100,7 @@ class Blizzard2012(Corpus):
         def map_fn(splitIndex, iterator):
             csv, max_len, count = reduce(
                 lambda acc, kv: (
-                "\n".join([acc[0], source_metadata_to_tsv(kv[1])]), max(acc[1], len(kv[1].text)), acc[2] + 1),
+                    "\n".join([acc[0], source_metadata_to_tsv(kv[1])]), max(acc[1], len(kv[1].text)), acc[2] + 1),
                 iterator, ("", 0, 0))
             filename = f"blizzard2012-source-metadata-{splitIndex:03d}.tsv"
             filepath = os.path.join(self.out_dir, filename)
--- visualize_alignment.py
+++ visualize_alignment.py
@@ -1,4 +1,35 @@
-# coding: utf-8
+# BSD 3-Clause License
+#
+# Copyright (c) 2018, Yamagishi Laboratory, National Institute of Informatics
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==============================================================================
+
+
 """
 usage: visualize_alignment.py [options] <filename>
 
@@ -16,6 +47,7 @@ from hparams import hparams
 
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+
 
 class TrainingResult(
     namedtuple("TrainingResult",
@@ -43,15 +75,17 @@ def read_training_result(filename):
         alignment_target_length = example.features.feature['alignment_target_length'].int64_list.value
 
         texts = (t.decode('utf-8') for t in text)
-        alignments = [np.frombuffer(align, dtype=np.float32).reshape([batch_size, src_len, tgt_len]) for align, src_len, tgt_len in
+        alignments = [np.frombuffer(align, dtype=np.float32).reshape([batch_size, src_len, tgt_len]) for
+                      align, src_len, tgt_len in
                       zip(alignment, alignment_source_length, alignment_target_length)]
         alignments = [[a[i] for a in alignments] for i in range(batch_size)]
         predicted_mels = (np.frombuffer(mel, dtype=np.float32).reshape([mel_len, mel_width]) for mel, mel_len in
-                zip(predicted_mel, predicted_mel_length))
+                          zip(predicted_mel, predicted_mel_length))
         ground_truth_mels = (np.frombuffer(mel, dtype=np.float32).reshape([mel_len, mel_width]) for mel, mel_len in
-                zip(ground_truth_mel, mel_length))
+                             zip(ground_truth_mel, mel_length))
 
-        for id, text, align, pred_mel, gt_mel, mel_length in zip(id, texts, alignments, predicted_mels, ground_truth_mels, mel_length_without_padding):
+        for id, text, align, pred_mel, gt_mel, mel_length in zip(id, texts, alignments, predicted_mels,
+                                                                 ground_truth_mels, mel_length_without_padding):
             yield TrainingResult(
                 global_step=global_step,
                 id=id,
@@ -79,7 +113,7 @@ def save_alignment(alignments, text, mel_length, _id, path, info=None):
             xlabel += '\n\n' + info
         ax.set_xlabel(xlabel)
         ax.set_ylabel('Encoder timestep')
-        ax.set_title("layer {}".format(i+1))
+        ax.set_title("layer {}".format(i + 1))
         ax.hlines(len(text), xmin=0, xmax=mel_length // hparams.outputs_per_step, colors=['red'])
         ax.vlines(mel_length // hparams.outputs_per_step, ymin=0, ymax=len(text), colors=['red'])
     fig.subplots_adjust(wspace=0.4, hspace=0.6)
@@ -97,4 +131,5 @@ if __name__ == "__main__":
     output_filename = prefix + output_base_filename + "_{}.png"
 
     for result in read_training_result(filename):
-        save_alignment(result.alignments, result.text, result.mel_length, result.id, os.path.join(output_dir, output_filename).format(result.id))
+        save_alignment(result.alignments, result.text, result.mel_length, result.id,
+                       os.path.join(output_dir, output_filename).format(result.id))
