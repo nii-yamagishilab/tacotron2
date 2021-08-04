ecf32388b8d168f73fd9c8abda791bb61712b09b
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2021-08-05 02:08:35 +0900

    support matplotlib 3

--- util/metrics.py
+++ util/metrics.py
@@ -36,11 +36,11 @@ def plot_mel(mel, mel_predicted, text, _id, global_step, filename):
     from matplotlib import pylab as plt
     fig = plt.figure(figsize=(16, 10))
     ax = fig.add_subplot(2, 1, 1)
-    im = ax.imshow(mel.T, origin="lower bottom", aspect="auto", cmap="magma")
+    im = ax.imshow(mel.T, origin="lower", aspect="auto", cmap="magma")
     fig.colorbar(im, ax=ax)
     ax = fig.add_subplot(2, 1, 2)
     im = ax.imshow(mel_predicted[:mel.shape[0], :].T,
-                   origin="lower bottom", aspect="auto", cmap="magma")
+                   origin="lower", aspect="auto", cmap="magma")
     fig.colorbar(im, ax=ax)
     fig.suptitle(f"record ID: {_id}\nglobal step: {global_step}\ninput text: {str(text)}")
     fig.savefig(filename, format='png')
@@ -51,11 +51,11 @@ def plot_spec(spec, spec_predicted, _id, global_step, filename):
     from matplotlib import pylab as plt
     fig = plt.figure(figsize=(16, 10))
     ax = fig.add_subplot(2, 1, 1)
-    im = ax.imshow(spec.T, origin="lower bottom", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
+    im = ax.imshow(spec.T, origin="lower", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
     fig.colorbar(im, ax=ax)
     ax = fig.add_subplot(2, 1, 2)
     im = ax.imshow(spec_predicted[:spec.shape[0], :].T,
-                   origin="lower bottom", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
+                   origin="lower", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
     fig.colorbar(im, ax=ax)
     fig.suptitle(f"record ID: {_id}\nglobal step: {global_step}")
     fig.savefig(filename, format='png')
@@ -81,20 +81,20 @@ def plot_predictions(alignments, mel, mel_predicted, spec, spec_predicted, text,
     fig.subplots_adjust(wspace=0.4, hspace=0.6)
 
     ax = fig.add_subplot(num_alignment + 4, 1, num_alignment + 1)
-    im = ax.imshow(mel.T, origin="lower bottom", aspect="auto", cmap="magma")
+    im = ax.imshow(mel.T, origin="lower", aspect="auto", cmap="magma")
     fig.colorbar(im, ax=ax)
     ax = fig.add_subplot(num_alignment + 4, 1, num_alignment + 2)
     im = ax.imshow(mel_predicted[:mel.shape[0], :].T,
-                   origin="lower bottom", aspect="auto", cmap="magma")
+                   origin="lower", aspect="auto", cmap="magma")
     fig.colorbar(im, ax=ax)
 
     if spec is not None and spec_predicted is not None:
         ax = fig.add_subplot(num_alignment + 4, 1, num_alignment + 3)
-        im = ax.imshow(spec.T, origin="lower bottom", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
+        im = ax.imshow(spec.T, origin="lower", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
         fig.colorbar(im, ax=ax)
         ax = fig.add_subplot(num_alignment + 4, 1, num_alignment + 4)
         im = ax.imshow(spec_predicted[:spec.shape[0], :].T,
-                       origin="lower bottom", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
+                       origin="lower", aspect="auto", cmap="magma", vmin=0.0, vmax=1.0)
         fig.colorbar(im, ax=ax)
 
     fig.suptitle(f"record ID: {_id}\ninput text: {str(text)}")
